b490d27c6304400d436d56eea989e5d21c8858a2
Ira Woodring <irawoodring@gmail.com>
2022-01-05 15:38:07 -0500

    Add ghost movement.

--- Assets/GhostController.cs
+++ Assets/GhostController.cs
@@ -19,6 +19,13 @@ public class GhostController : MonoBehaviour
     void OnTriggerEnter(Collider other){
         transform.position -= transform.forward * 1.0f;
         //transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.AngleAxis(180, Vector3.up), 10 * Time.deltaTime);
-        transform.Rotate(0, 90, 0);
+        double val = Random.value;
+        if(val < .33){
+            transform.Rotate(0, 90, 0);
+        } else if(val < .66){
+            transform.Rotate(0, -90, 0);
+        } else {
+            transform.Rotate(0, 180, 0);
+        }
     }
 }
